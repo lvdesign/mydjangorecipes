700effae4b490c36ff14947f2610c5888d72c701
LVdesign <mailbox@lvdesign.com.fr>
2025-08-28 10:08:39 +0200

    user authentication

--- myrecipes/settings.py
+++ myrecipes/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "myrecipes.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],  # new,
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+# direction
+LOGIN_REDIRECT_URL = "home"  # new
+LOGOUT_REDIRECT_URL = "home"  # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
